9bcc2168ebb4abf01cd84c6b284a1aa837a5eca4
Rob Johnson <rob.johnson87@ntlworld.com>
2020-10-19 12:54:23 +0100

    tidy

--- make_alignment.R
+++ make_alignment.R
@@ -26,42 +26,42 @@ setwd(region)
 print(region)
   
   
-  fastafn <- paste0("algn3.fasta")
+fastafn <- paste0("algn3.fasta")
   
-  # Load the edited gisaid metadata 
-  md <- read.csv( '../datasets/msa_0902/meta.csv', stringsAs=FALSE ) 
-  if(!region%in%c(unique(md$Country),unique(md$RegionOrState),unique(md$CityOrCounty))) 
-    stop(paste0('\n"',region,'" is not among cities, counties, countries, regions and states.\n\n'))
+# Load the edited gisaid metadata 
+md <- read.csv( '../datasets/msa_0902/meta.csv', stringsAs=FALSE ) 
+if(!region%in%c(unique(md$Country),unique(md$RegionOrState),unique(md$CityOrCounty))) 
+  stop(paste0('\n"',region,'" is not among cities, counties, countries, regions and states.\n\n'))
   
-  ## path to the GISAID alignment: 
-  gisaid_file <- '../datasets/msa_0902/gisaid.fas'
-  ## A path to the file containing small genetic distance pairs
-  distfn = '../datasets/data_0820/tn93.txt'
+## path to the GISAID alignment: 
+gisaid_file <- '../datasets/msa_0902/gisaid.fas'
+## A path to the file containing small genetic distance pairs
+distfn = '../datasets/data_0820/tn93.txt'
   
-  # define some parameters 
-  ## When do internal SEIR dynamics initiate: 
-  startTime = 2020.15
-  ## How many sequences to include within the region?
-  n_region = 1000
-  ## How many to include from the international reservoir?
-  n_reservoir = 50 # (actual number to be included will be greater since it also includes close distance matches)
-  ## How many starting trees? A BEAST run will be carried out for each 
-  n_startingtrees = 1
+# define some parameters 
+## When do internal SEIR dynamics initiate: 
+startTime = 2020.15
+## How many sequences to include within the region?
+n_region = 1000
+## How many to include from the international reservoir?
+n_reservoir = 50 # (actual number to be included will be greater since it also includes close distance matches)
+## How many starting trees? A BEAST run will be carried out for each 
+n_startingtrees = 1
   
   
-  # Set dedup = TRUE in region sampler and exog sampler to remove duplicate sequences
-    md2 <- read.csv(distfn,sep=',',stringsAsFactors = F)
-    regiontips = region_sampler1( md, n = n_region  , inclusion_rules = list( c('CityOrCounty', paste0('^',region,'$')),
+# Set dedup = TRUE in region sampler and exog sampler to remove duplicate sequences
+md2 <- read.csv(distfn,sep=',',stringsAsFactors = F)
+regiontips = region_sampler1( md, n = n_region  , inclusion_rules = list( c('CityOrCounty', paste0('^',region,'$')),
                                                                               c('RegionOrState', paste0('^',region,'$')),
                                                                               c('Country', paste0('^',region,'$'))), dedup = FALSE, time_stratify=F)
-    # Sample a set of closely related external sequences, 
-    exogtips = exog_sampler2( md, n=n_reservoir, smallGDpairs=md2, region_sample=regiontips, exclusion_rules = list( c('CityOrCounty', paste0('^',region,'$')),
+# Sample a set of closely related external sequences, 
+exogtips = exog_sampler2( md, n=n_reservoir, smallGDpairs=md2, region_sample=regiontips, exclusion_rules = list( c('CityOrCounty', paste0('^',region,'$')),
                                                                                                                      c('RegionOrState', paste0('^',region,'$')),
                                                                                                                      c('Country', paste0('^',region,'$')) ), dedup= FALSE )
-    # You could select based on a different geographic criterion, current fields in metadata are  "Continent" "Country" "RegionOrState" "CityOrCounty"
-    rm(md2)
-    #~ Here is a subset of the metadata that corresponds to the sample. Check this carefully- is the sample including everything you want and not including things you dont want?
-    mdregion = md[ match( regiontips, md$seqName ) , ]
+# You could select based on a different geographic criterion, current fields in metadata are  "Continent" "Country" "RegionOrState" "CityOrCounty"
+rm(md2)
+#~ Here is a subset of the metadata that corresponds to the sample. Check this carefully- is the sample including everything you want and not including things you dont want?
+mdregion = md[ match( regiontips, md$seqName ) , ]
     
 
 
@@ -84,8 +84,7 @@ prep_tip_labels_seijr <- function (algnfn, outfn, regiontips, exogtips, metadata
   invisible(dd)
 }
 
-# Now make the aligment for BEAST. This adds sample time and deme (internal or external) to tip labels:
-d3 = prep_tip_labels_seijr(algnfn=gisaid_file, outfn=fastafn, regiontips=regiontips, exogtips=exogtips, metadata=md  )
+prep_tip_labels_seijr(algnfn=gisaid_file, outfn=fastafn, regiontips=regiontips, exogtips=exogtips, metadata=md  )
 
 rm(md)
   
